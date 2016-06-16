41d17d2715fc8b3da8f11e01d4d5dea9cc9cbc12
Guilhermeslucas <guilherme.slucas@gmail.com>
2016-06-16 20:09:26 -0300

    coloca pontuacao no bcd

--- size_counter.vhd
+++ size_counter.vhd
@@ -5,11 +5,16 @@ USE ieee.numeric_std;
 -- tamanho da cobra
 ENTITY size_counter IS
     PORT (eaten      : IN STD_LOGIC;
-          snake_size : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+          snake_size : BUFFER  STD_LOGIC_VECTOR(7 DOWNTO 0));
 END size_counter;
 
 ARCHITECTURE Behavior OF size_counter IS
-	SIGNAL counter : integer range 0 to 255 := 0;
+    COMPONENT conv_7seg IS
+        PORT (digit: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+                seg: OUT STD_LOGIC_VECTOR(6 DOWNTO 0));
+        END COMPONENT;
+
+    SIGNAL counter : integer range 0 to 255 := 0;
 	
 BEGIN
 	PROCESS(eaten)
@@ -19,4 +24,5 @@ BEGIN
 	END PROCESS;
 	
 	snake_size <= std_logic_vector(to_unsigned(counter, snake_size'LENGTH));
+        saida_contador: conv_7seg PORT MAP (snake_size, seg); 
 END Behavior;
